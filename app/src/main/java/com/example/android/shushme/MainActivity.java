2c4ff9c3307fd1393339dd7a4400ea1bb6ded2a8
Ruslan Iusupov <ruslanyussupov@gmail.com>
2018-03-31 19:43:53 +0300

    Done

--- app/src/main/java/com/example/android/shushme/MainActivity.java
+++ app/src/main/java/com/example/android/shushme/MainActivity.java
@@ -16,13 +16,19 @@ package com.example.android.shushme;
 * limitations under the License.
 */
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -34,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     // Constants
     public static final String TAG = MainActivity.class.getSimpleName();
+    private static final int PERMISSION_REQUEST_CODE = 111;
 
     // Member variables
     private PlaceListAdapter mAdapter;
@@ -81,14 +88,44 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     }
 
     // COMPLETED (5) Override onConnected, onConnectionSuspended and onConnectionFailed for GoogleApiClient
-    // TODO (7) Override onResume and inside it initialize the location permissions checkbox
-    // TODO (8) Implement onLocationPermissionClicked to handle the CheckBox click event
-    // TODO (9) Implement the Add Place Button click event to show  a toast message with the permission status
+    // COMPLETED (7) Override onResume and inside it initialize the location permissions checkbox
+    // COMPLETED (8) Implement onLocationPermissionClicked to handle the CheckBox click event
+    // COMPLETED (9) Implement the Add Place Button click event to show  a toast message with the permission status
 
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        CheckBox permissionCheckBox = findViewById(R.id.permission_checkbox);
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            permissionCheckBox.setChecked(true);
+            permissionCheckBox.setEnabled(false);
+        } else {
+            permissionCheckBox.setChecked(false);
+        }
+
+    }
+
+    public void onLocationPermissionClicked(View view) {
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+
+    }
+
+    public void onAddLocationClicked(View view) {
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, getString(R.string.permission_granted), Toast.LENGTH_SHORT)
+                    .show();
+        } else {
+            Toast.makeText(this, getString(R.string.need_permission), Toast.LENGTH_SHORT)
+                    .show();
+        }
 
     }
 }
